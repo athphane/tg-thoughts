2014d029deb0cb5c0edea72416dfd0325a0be642
athphane <datahoarder@athfan.com>
2021-04-28 10:49:35 +0500

    upload

--- pyroskeletonbot/__main__.py
+++
@@ -1,9 +0,0 @@
-import pyroskeletonbot
-from pyroskeletonbot import PyroSkeletonBot, scheduler
-
-if __name__ == '__main__':
-    pyroskeletonbot.client = PyroSkeletonBot
-
-    scheduler.start()
-
-    PyroSkeletonBot.run()
